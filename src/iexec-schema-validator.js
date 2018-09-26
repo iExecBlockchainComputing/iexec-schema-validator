44a4e3e50f51c06163c12af11bb9055898728eff
victor <victor@vbonhomme.fr>
2018-09-26 14:32:20 +0200

    rework for sdk integration

--- src/iexec-schema-validator.js
+++ src/iexec-schema-validator.js
@@ -1,63 +1,126 @@
-// const Debug = require('debug');
+const Debug = require('debug');
 const Joi = require('joi');
-const semver = require('semver');
 
-// const debug = Debug('iexec-schema-validator');
+const debug = Debug('iexec-schema-validator');
 
-const myJoi = Joi.extend({
-  base: Joi.string(),
-  name: 'string',
-  language: {
-    semver: 'needs to be a valid semver version',
-  },
-  rules: [{
-    name: 'semver',
-    validate(params, value, state, options) {
-      if (!semver.valid(value)) {
-        return this.createError('string.semver', { v: value }, state, options);
-      }
-      return value;
-    },
-  }],
-});
-
-const githubSchema = Joi.object({
-  version: myJoi.string().semver().required(),
-  description: Joi.string().min(150).max(2000).required(),
-  license: Joi.string().required(),
-  homepage: Joi.string().required(),
+const baseSchema = Joi.object({
+  type: Joi.string(),
+  description: Joi.string()
+    .min(150)
+    .max(2000)
+    .required(),
   logo: Joi.string().required(),
-  author: Joi.object({
-    name: Joi.string().min(1).max(40).required(),
-    email: Joi.string().email(),
+  social: Joi.object({
+    website: Joi.string(),
+    github: Joi.string(),
   }).required(),
-  chains: Joi.object(),
-  social: Joi.object(),
-  work: Joi.string().required(),
-  updatedAt: Joi.string().isoDate().required(),
+  addresses: Joi.object(),
+  repo: Joi.string().required(),
 });
 
-const fileDBSchema = Joi.object().keys({
-  type: Joi.string().required().valid('dapp'),
-  name: Joi.string().min(1).max(40).required(),
-  branch: Joi.string().required(),
-  price: Joi.number().integer().positive().required(),
-  created: Joi.string().isoDate().required(),
+const dappSchema = baseSchema.append({
+  license: Joi.string().required(),
+  author: Joi.string().required(),
+  app: Joi.object({
+    name: Joi.string().required(),
+    price: Joi.number()
+      .integer()
+      .greater(-1),
+    params: Joi.object().required(),
+  }).required(),
+  order: Joi.object(),
 });
 
-const validateGithub = (obj) => {
-  const result = githubSchema.validate(obj);
-  if (result.error) throw Error(result.error.details.map(e => e.message).join(' + '));
-  return true;
-};
+const poolSchema = baseSchema.append({
+  workerPool: Joi.object({
+    description: Joi.string().required(),
+    subscriptionLockStakePolicy: Joi.number()
+      .integer()
+      .greater(-1),
+    subscriptionMinimumStakePolicy: Joi.number()
+      .integer()
+      .greater(-1),
+    subscriptionMinimumScorePolicy: Joi.number()
+      .integer()
+      .greater(-1),
+  }).required(),
+});
 
-const validateFileDB = (obj) => {
-  const result = fileDBSchema.validate(obj);
-  if (result.error) throw Error(result.error.details.map(e => e.message).join(' + '));
+const registryEntrySchema = Joi.object().keys({
+  name: Joi.string()
+    .min(1)
+    .max(40),
+  org: Joi.string()
+    .min(1)
+    .max(40)
+    .required(),
+  created: Joi.string()
+    .isoDate()
+    .required(),
+});
+
+const partnerSchema = registryEntrySchema.append({
+  description: Joi.string()
+    .min(150)
+    .max(2000)
+    .required(),
+  logo: Joi.string().required(),
+  license: Joi.string(),
+  social: Joi.object({
+    website: Joi.string(),
+    github: Joi.string(),
+    linkedin: Joi.string(),
+    twitter: Joi.string(),
+    medium: Joi.string(),
+  }).required(),
+  type: Joi.string().required(),
+  link: Joi.string(),
+  buttonText: Joi.string(),
+  theme: Joi.string(),
+  button: Joi.boolean(),
+});
+
+const chainConfSchema = Joi.object({
+  host: Joi.string().required(),
+  id: Joi.string().required(),
+  scheduler: Joi.string().required(),
+  hub: Joi.string(),
+});
+
+const chainsConfSchema = Joi.object({
+  default: Joi.string(),
+  chains: Joi.object()
+    .pattern(/^/, chainConfSchema)
+    .required(),
+});
+
+const accountConfSchema = Joi.object({
+  jwtoken: Joi.string().required(),
+});
+
+const walletConfSchema = Joi.object({
+  privateKey: Joi.string().required(),
+  publicKey: Joi.string().required(),
+  address: Joi.string().required(),
+});
+
+const validateObj = schema => (obj, { strict = true } = {}) => {
+  const result = schema.validate(obj);
+  if (result.error) {
+    debug('validateObj()', result.error);
+    if (strict) throw Error(result.error.details.map(e => e.message).join(' + '));
+    return false;
+  }
   return true;
 };
 
 module.exports = {
-  validateGithub,
-  validateFileDB,
+  validateRegistryEntry: validateObj(registryEntrySchema),
+  validateDapp: validateObj(dappSchema),
+  validatePool: validateObj(poolSchema),
+  validatePartner: validateObj(partnerSchema),
+  validateChainConf: validateObj(chainConfSchema),
+  validateChainsConf: validateObj(chainsConfSchema),
+  validateAccountConf: validateObj(accountConfSchema),
+  validateWalletConf: validateObj(walletConfSchema),
 };
