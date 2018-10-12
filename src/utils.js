b152a4d10d8929ce1e921405ffcf6273abed45ed
victor <victor@vbonhomme.fr>
2018-10-12 15:00:42 +0200

    lint

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,16 @@
-const keccak256 = require('js-sha3').keccak_256;
+const keccak256 = require("js-sha3").keccak_256;
 
-const isChecksumAddress = (_address) => {
+const isChecksumAddress = _address => {
   // Check each case
-  const address = _address.replace('0x', '');
+  const address = _address.replace("0x", "");
   const addressHash = keccak256(address.toLowerCase());
   for (let i = 0; i < 40; i += 1) {
     // the nth letter should be uppercase if the nth digit of casemap is 1
     if (
-      (parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i])
-      || (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])
+      (parseInt(addressHash[i], 16) > 7 &&
+        address[i].toUpperCase() !== address[i]) ||
+      (parseInt(addressHash[i], 16) <= 7 &&
+        address[i].toLowerCase() !== address[i])
     ) {
       return false;
     }
@@ -16,12 +18,15 @@ const isChecksumAddress = (_address) => {
   return true;
 };
 
-const isETHAddress = (address) => {
+const isETHAddress = address => {
   if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
     // check if it has the basic requirements of an address
     return false;
   }
-  if (/^(0x)?[0-9a-f]{40}$/.test(address) || /^(0x)?[0-9A-F]{40}$/.test(address)) {
+  if (
+    /^(0x)?[0-9a-f]{40}$/.test(address) ||
+    /^(0x)?[0-9A-F]{40}$/.test(address)
+  ) {
     // If it's all small caps or all all caps, return true
     return true;
   }
@@ -30,5 +35,5 @@ const isETHAddress = (address) => {
 };
 
 module.exports = {
-  isETHAddress,
+  isETHAddress
 };
